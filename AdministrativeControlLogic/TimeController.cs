e97b4d851a326d8bebc49a4dbfdaec832dba3500
SlavyanSon <slava@tomilov.net>
2023-02-20 19:26:46 +0500

    базовый код для отслеживания времени бездействия пользователя
    и тесты

---
+++ AdministrativeControlLogic/TimeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AdministrativeControlLogic
+{
+    public class TimeController
+    {
+        internal struct LASTINPUTINFO
+        {
+            public uint cbSize;
+
+            public uint dwTime;
+        }
+
+        
+
+    }
+}
---
+++ AdministrativeControlLogicTests/IdleTimeFinderTests.cs
@@ -0,0 +1,31 @@
+﻿using AdministrativeControlLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministrativeControlLogic.Tests
+{
+    [TestClass()]
+    public class IdleTimeFinderTests
+    {
+        [TestMethod()]
+        public void GetIdleTimeTest()
+        {
+            var ticks = IdleTimeFinder.GetIdleTime();
+            Console.WriteLine(ticks);
+            Assert.IsTrue(ticks > 0);
+        }
+   
+        [TestMethod()]
+        public void GetLastInputTimeTest()
+        {
+            var ticks = IdleTimeFinder.GetLastInputTime();
+            var timespent = TimeSpan.FromMilliseconds(ticks);
+            Console.WriteLine(timespent);
+            Assert.IsTrue(ticks > 0);
+        }
+    }
+}
